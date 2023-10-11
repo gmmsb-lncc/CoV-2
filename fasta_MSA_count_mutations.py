e82cd1f480d87ced5853df0b679a87552b302cd3
Leon Sulfierry <leon.bio0@hotmail.com>
2023-10-11 16:08:43 -0300

    fasta_MSA_count_mutations.py

--- fasta_MSA_count_mutations.py
+++ fasta_MSA_count_mutations.py
@@ -1,70 +1,56 @@
-# uso: python3 script.py msa_input.fasta  output_count.csv
-
 import sys
 import numpy as np
 import pandas as pd
 from Bio import AlignIO
 
-def variations_count(alignment, result_variations):
+class MutationCount:
     
-    matrix = np.array(alignment)
-    rows, cols = matrix.shape
+    def __init__(self, alignment):
+        self.matrix = np.array(alignment)
+        
+    def _get_variation_matrix(self):
+        rows, cols = self.matrix.shape
 
-    list_mat = []
-    for j in range(cols):
-        list_row = []
-        list_row.append(matrix[0][j])
-        for i in range(1,rows):
-            if matrix[i][j] != matrix[0][j]:
-                list_row.append(matrix[i][j])
-            else:
-                list_row.append('0')
+        list_mat = []
+        for j in range(cols):
+            list_row = [self.matrix[0][j]]
+            for i in range(1, rows):
+                list_row.append(self.matrix[i][j] if self.matrix[i][j] != self.matrix[0][j] else '0')
+            list_mat.append(list_row)
 
+        return np.array(list_mat).transpose()
 
-        list_mat.append(list_row)
+    def variations_count(self):
+        matrix_temp = self._get_variation_matrix()
+        df = pd.DataFrame(matrix_temp)
 
-    matrix_temp = np.array(list_mat)
-    matrix_temp = matrix_temp.transpose()
-    df = pd.DataFrame(matrix_temp)
+        rows, cols = matrix_temp.shape
+        l_changes, l_qtd, l_row_changes = [], [], []
 
-    result = []
-    for col in df.columns:
-        result.append(df[col].value_counts())
+        for j in range(cols):
+            for i in range(1, rows):
+                if matrix_temp[i][j] not in ('X', '0', '-'):  # Ignoring 'X', '0', and gaps '-'
+                    str_change = matrix_temp[0][j] + str(j + 1) + matrix_temp[i][j]
+                    if str_change not in l_changes:
+                        l_changes.append(str_change)
+                        l_qtd.append(1)
+                        l_row_changes.append(str(i + 1))
+                    else:
+                        idx = l_changes.index(str_change)
+                        l_qtd[idx] += 1
+                        l_row_changes[idx] += ',' + str(i + 1)
 
-    for row in result:
-        print(row)
+        df_result = pd.DataFrame({
+            'Changes': l_changes,
+            'Times': l_qtd,
+            'Lines': l_row_changes
+        })
 
-    # Create the new table
-    rows, cols = matrix_temp.shape
-    l_changes = []
-    l_qtd = []
-    l_row_changes = []
+        print(df_result)
+        return df_result
 
-
-    for j in range(cols):
-        for i in range(1,rows):
-            if (matrix_temp[i][j] != 'X') and (matrix_temp[i][j] != '0') and (matrix_temp[i][j] != '-'): # para desconsiderar o gap '-'
-                str_change = matrix_temp[0][j]+str((j+1))+matrix_temp[i][j]
-                if str_change not in l_changes:
-                    l_changes.append(str_change)
-                    l_qtd.append(1)
-                    l_row_changes.append(str(i+1))
-                else:
-                    idx = l_changes.index(str_change)
-                    l_qtd[idx] = l_qtd[idx]+1
-                    l_row_changes[idx] = l_row_changes[idx]+','+str(i+1)
-
-    df_result = pd.DataFrame()
-    df_result['Changes'] = l_changes
-    df_result['Times'] = l_qtd
-    df_result['Lines'] = l_row_changes
-    print(df_result)
-    
-    return df_result.to_csv(result_variations, index=False)
-    
-
-if __name__=="__main__":
+if __name__ == "__main__":
     alignment = AlignIO.read(sys.argv[1], 'fasta')
-    result_variations = sys.argv[2]
-    
-    variations_count(alignment, result_variations)
+    mutation_counter = MutationCount(alignment)
+    result = mutation_counter.variations_count()
+    result.to_csv(sys.argv[2], index=False)
