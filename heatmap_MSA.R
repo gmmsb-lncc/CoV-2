ac6d026de13b9f6760082c5c7938bde99b2b16ba
Leon Sulfierry <leon.bio0@hotmail.com>
2023-10-11 16:12:31 -0300

    heatmap_MSA.R

--- heatmap_MSA.R
+++ heatmap_MSA.R
@@ -2,37 +2,34 @@ library(ggmsa)
 library(RColorBrewer)
 library(pals)
 
+msa_heatmap <- function(sequences, pos_start, pos_end) {
+  my_pal <- colorRampPalette(rev(brewer.pal(n = 9, name = "Reds")))
+  my_custom <- data.frame(
+    names = c(LETTERS[1:26], "-"), 
+    color = my_pal(27), 
+    stringsAsFactors = FALSE
+  )
+  
+  pals::pal.bands(my_custom$color)
+  
+  res_msa <- ggmsa(sequences, 
+                   pos_start, 
+                   pos_end, 
+                   custom_color = my_custom,
+                   by_conservation = TRUE,
+                   border = "white",
+                   seq_name = TRUE,
+                   show.legend = TRUE) + 
+    geom_seqlogo(color = "Chemistry_AA") +
+    geom_msaBar()
+  
+  return(res_msa)
+}
 
-# heatmap range
+# Input and parameters
 sequences = "./canonical_consensus/VARIANTS_canonical_consensus_MSA.fasta"
 pos_start = 415
 pos_end   = 502
 
-msa_heatmap <- function(sequences, pos_start, pos_end){
-  
-  my_pal <- colorRampPalette(rev(brewer.pal(n = 9, name = "Reds")))
-  my_cutstom <- data.frame(names = c(LETTERS[1:26],"-"), 
-                           color = my_pal(27), 
-                           stringsAsFactors = FALSE)
-  head(my_cutstom)
-  
-  pals::pal.bands(my_cutstom$color)
-  
-  res_msa <- ggmsa(sequences, 
-                  pos_start, 
-                  pos_end, 
-        #char_width = 0,
-                  font = NULL,
-                  custom_color = my_cutstom,
-                  by_conservation = TRUE,
-                  border = "white",
-                  seq_name = TRUE,
-                  show.legend = TRUE) + 
-                  geom_seqlogo(color = "Chemistry_AA") +
-                  geom_msaBar()
-  
-  return(res_msa)  
-}
-
+# Execute function
 msa_heatmap(sequences, pos_start, pos_end)
-
